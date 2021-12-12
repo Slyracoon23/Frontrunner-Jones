48af3c5541c8bbffd60e08e59984388dd6a60ac6
Earl Potters <earl.potters@gmail.com>
2021-12-11 19:37:19 -0700

    added multitprocessing

--- test.py
+++ test.py
@@ -3,9 +3,15 @@ import sys
 import pymongo
 import time
 from itertools import permutations
+from itertools import repeat
+import bson
+from web3.types import LatestBlockParam
 
 from emulator import Emulator
 from web3 import Web3
+
+import multiprocessing
+from multiprocessing import Pool
 
 
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
@@ -26,25 +32,64 @@ def init_w3():
     global mongo_connection
 
 
-    w3 = Web3(Web3.WebsocketProvider(WEB3_WS_PROVIDER))
+    w3 = Web3(Web3.HTTPProvider(WEB3_HTTP_PROVIDER))
     if w3.isConnected():
         print("Connected worker to "+w3.clientVersion)
     else:
-        print(colors.FAIL+"Error: Could not connect to "+WEB3_WS_PROVIDER+colors.END)
+        print(colors.FAIL+"Error: Could not connect to "+WEB3_HTTP_PROVIDER+colors.END)
 
     mongo_connection = pymongo.MongoClient("mongodb://"+MONGO_HOST+":"+str(MONGO_PORT), maxPoolSize=None)
 
+# def analysze_transaction_order(txs, block_number):
+#     print(f'txs: {txs} and block_number: {block_number}')
 
 
-def analysze_transaction_order(txs, block_number):
+def chunks(l, n):
+    for i in range(0, len(list(l)), n):
+        yield l[i:i + n]
+
+def analysze_transaction_batch(list_of_txs, block_data):
+    print(f'Anayszing Batch')
+
+    ########################################## 
+    # Set up mongoDB client
+    mongo_connection_internal = pymongo.MongoClient("mongodb://"+MONGO_HOST+":"+str(MONGO_PORT), maxPoolSize=None)
+    ##########################################
+
+    findings = []
+
+    emu = Emulator(WEB3_HTTP_RPC_HOST, WEB3_HTTP_RPC_PORT, block_data)
+
+    block_number = block_data["number"] + 1
+
+    for txs in list_of_txs:
+        
+        emu.take_snapshot()
+
+        finding = analysze_transaction_order(txs, emu, block_number)
+
+
+        findings.append(finding)
+
+        emu.restore_from_snapshot()
+
+
+    collection = mongo_connection_internal["front_running"]["MEV_results"]
+    collection.insert_many(findings)
+
+    print(f'Finished Batch')
+
+
+
+
+def analysze_transaction_order(txs, emu, block_number):
+
     start = time.time()
 
     transaction_list = []
 
     list_of_erc20_and_eth_transfers = []
 
-
-    emu = Emulator(WEB3_HTTP_RPC_HOST, WEB3_HTTP_RPC_PORT, w3.eth.getBlock(block_number - 1, False))
 
 
     # Execute transaction 
@@ -66,13 +111,16 @@ def analysze_transaction_order(txs, block_number):
                 token_address = '0x' + log[0].hex()
                 _from = hex(log[1][1])
                 _to = hex(log[1][2])
+                # don't know why this is not making 8-byte int to Long
+                # _value = bson.Int64(int(log[2].hex(), 16))
+                _value = hex(int(log[2].hex(), 16))
 
-                list_of_erc20_and_eth_transfers.append({"token_address": token_address, "from":_from, "to":_to})
+                list_of_erc20_and_eth_transfers.append({"token_address": token_address, "from":_from, "to":_to , "value":_value })
 
     # save in MongoDB database
 
     end = time.time()
-    print(f'Execution_time analysze_transaction_orderanalysze_transaction_order: {end-start}')
+    # print(f'Execution_time analysze_transaction_orderanalysze_transaction_order: {end-start}')
     finding = {
         "block_number": block_number,
         "transaction_hash_list": [ tx["hash"].hex() for tx in txs],
@@ -80,8 +128,12 @@ def analysze_transaction_order(txs, block_number):
         "execution_time": float(end-start)
     }
 
-    collection = mongo_connection["front_running"]["MEV_results"]
-    collection.insert_one(finding)
+    return finding
+
+    # print(f'Findings: {finding}')
+
+
+    
 
 
 
@@ -133,15 +185,26 @@ def analysze_block_range(block_range):
             txs = list(filter(lambda tx: is_simple_erc20_transfer(tx) == False, txs))
 
             # permutate over all possiblities
-            txs_permutations = permutations(txs,2)
-            for perm_txs in txs_permutations:
-                analysze_transaction_order(perm_txs, block_number)
+            txs_permutations = list(permutations(txs,2))
+            
+            block_data = w3.eth.getBlock(block_number - 1, False)
+
+
+
+            chucks_txs_permutations = chunks(txs_permutations, len(txs_permutations)//16)
+
+            with Pool(processes=multiprocessing.cpu_count()) as pool:
+                pool.starmap(analysze_transaction_batch, zip(chucks_txs_permutations, repeat(block_data)))
+
+            # for chuck_txs_permutations in chucks_txs_permutations:
+            #     analysze_transaction_batch(chuck_txs_permutations, block_data)
 
             end = time.time()
-            print(f'Execution_time block: {end-start}')
+            print(f'Execution_time block@{block_number}: {end-start}')
 
             collection = mongo_connection["front_running"]["MEV_status"]
             collection.insert_one({"block_number": block_number, "execution_time": end-start})
+
 
 
 
